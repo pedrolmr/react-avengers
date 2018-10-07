47e3fd51f8efeb8924782dfbaa22c9fd124b65a6
pedro <pedroluismontesinosrivera@gmail.com>
2018-10-06 20:46:27 -0400

    created a avengersData.js file to store the data

---
+++ src/avengersData.js
@@ -0,0 +1,33 @@
+const Avengers = [
+    {
+        id: 1,
+        character: "Thor",
+        description: "Thor is a fictional superhero appearing in American comic books published by Marvel Comics. The character, based on the Norse deity of the same name, is the Asgardian god of thunder and possesses the enchanted hammer Mjolnir, which grants him the ability to fly and manipulate weather amongst his other superhuman attributes."
+    },
+    {
+        id: 2,
+        character: "Wasp",
+        description: "The Wasp (Janet van Dyne) is a fictional superheroine appearing in American comic books published by Marvel Comics. Created by Stan Lee, Ernie Hart and Jack Kirby, the character first appeared in Tales to Astonish #44 (June 1963). She is usually depicted as having the ability to shrink to a height of several centimeters, fly by means of insectoid wings, and fire bioelectric energy blasts. She is a founding member of the Avengers as well as a long time leader of the team."
+    },
+    {
+        id: 3,
+        character: "Iron Man",
+        description: "Anthony Edward Stark is a character portrayed by Robert Downey Jr. in the Marvel Cinematic Universe film franchise, based on the Marvel Comics character of the same name and known commonly by his alter ego, Iron Man."
+    },
+    {
+        id: 4,
+        character: "Hulk",
+        description: "The Hulk is a fictional superhero appearing in American comic books published by Marvel Comics. Created by writer Stan Lee and artist Jack Kirby, the character first appeared in the debut issue of The Incredible Hulk."
+    },
+    {
+        id: 5,
+        character: "Captain America",
+        description: "Captain America is a fictional superhero appearing in American comic books published by Marvel Comics. Created by cartoonists Joe Simon and Jack Kirby, the character first appeared in Captain America Comics #1 from Timely Comics, a predecessor of Marvel Comics."
+    },
+    {
+        id: 6,
+        character: "Spider-Man",
+        description: "Spider-Man is a fictional superhero created by writer-editor Stan Lee and writer-artist Steve Ditko. He first appeared in the anthology comic book Amazing Fantasy #15 in the Silver Age of Comic Books. "
+    },
+];
+export default Avengers;
